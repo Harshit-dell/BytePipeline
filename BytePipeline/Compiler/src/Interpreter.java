73090c4ba7ac462557cd3fb1749b9a4a6ce7e086
Harshit-dell <harshitsahu967@gmail.com>
2025-08-20 01:27:24 +0530

    Update Interpreter.java
    Recursion through the ATS tree

--- BytePipeline/Compiler/src/Interpreter.java
+++ BytePipeline/Compiler/src/Interpreter.java
@@ -1,3 +1,19 @@
-public class Interpreter{
-    //take AST and iterate thorw it
+public class  Interpreter {
+    public int evaluate(Expr expr) {
+        if (expr instanceof NumberExpr) {
+            return ((NumberExpr) expr).value;
+        }
+        int left=evaluate(((BinaryExpr)expr).left);
+        int right=evaluate(((BinaryExpr)expr).right);
+        switch (((BinaryExpr)expr).operator){
+                case "+":
+                    return  left+right;
+            case "-":
+                    return  left-right;
+        }
+        throw new RuntimeException("dikkat hai interpretor mein ");
+    }
+    public void execute(PrintStmt stmt) {
+        System.out.println(evaluate(stmt.expression));
+    }
 }
