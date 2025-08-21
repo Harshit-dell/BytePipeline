62bfbc831f80a4c20a42876e01682e6124769e6e
Harshit-dell <harshitsahu967@gmail.com>
2025-08-21 13:11:57 +0530

    Update Main.java

--- BytePipeline/Compiler/src/Main.java
+++ BytePipeline/Compiler/src/Main.java
@@ -1,19 +1,32 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("write code here:)");
-        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Working)");
+        String file_location="<add location here>first_program.txt";
+        BufferedReader reader=new BufferedReader(new FileReader(file_location));
         String value=reader.readLine();
-
-        Lexer lexer = new Lexer(value);
-        Parser parser = new Parser(lexer);
-
-        PrintStmt stmt = parser.parsePrint();
-
+        //interepreter hashmap was getting formated every time because it was in loop
         Interpreter interpreter=new Interpreter();
-        interpreter.execute(stmt);
+        while(value!=null){
+            if(value.trim().isEmpty()){
+                value=reader.readLine();
+                continue;
+            }
+            Lexer lexer = new Lexer(value);
+            Parser parser = new Parser(lexer);
+
+            Stmt stmt = parser.starter();
+
+            interpreter.execute(stmt);
+            value=reader.readLine();
+
+
+        }
+
+        reader.close();
     }
 }
