d28a6d56e7057ceab94c0ecb7816486e154f21bf
Harshit Sahu <harshitsahu967@gmail.com>
2025-08-22 16:53:20 +0530

    From last time code has bug that send token of EOF after LCURL

--- Compiler/src/Lexer.java
+++ Compiler/src/Lexer.java
@@ -12,18 +12,24 @@ public class Lexer {
         // this is null character ASSI value 0
         return  input.charAt(position);
     }
-
-
+    //debugger
+    private Token makeToken(Token.Type type,String value){
+        Token t=new Token(type,value);
+        System.out.println("TOKEN: " + t.type + t.value);
+        return t;
+    }
 
     //TODO this is main file
     public Token nextToken(){
         while(Character.isWhitespace(peek())){
             position++;
         }
+
+
 //check if empty
         char current=peek();
         if(current=='\0'){
-            return new Token(Token.Type.EOF,"");
+            return makeToken(Token.Type.EOF,"");
         }
 //NUMBER
         if(Character.isDigit(current)){
@@ -32,20 +38,20 @@ public class Lexer {
                 builder.append(peek());
                 position++;
             }
-            return new Token(Token.Type.NUMBER,builder.toString());
+            return makeToken(Token.Type.NUMBER,builder.toString());
         }
 
 
         //Character
         if(Character.isLetter(current)){
-           StringBuilder builder =new StringBuilder();
+            StringBuilder builder =new StringBuilder();
             while(Character.isLetterOrDigit(peek()) || peek()=='_'){
                 builder.append(peek());
                 position++;
             }
             String value=builder.toString();
             if(value.equals("tell")){
-                return  new Token(Token.Type.PRINT,"tell");
+                return  makeToken(Token.Type.PRINT,"tell");
             }
 //            else if(builder.equals("agar")){
 //                return  new Token(Token.Type.IF,"warna");
@@ -54,7 +60,7 @@ public class Lexer {
 //                return  new Token(Token.Type.ELSE,"warna");
 //            }
             else {
-                return  new Token(Token.Type.VARIABLE,value);
+                return  makeToken(Token.Type.VARIABLE,value);
             }
 
         }
@@ -62,18 +68,18 @@ public class Lexer {
 
 
         switch (current) {
-            case '+': position++;return new Token(Token.Type.PLUS, "+");
-            case '-': position++; return new Token(Token.Type.MINUS, "-");
-            case '*': position++; return new Token(Token.Type.STAR, "*");
-            case '/': position++; return new Token(Token.Type.SLASH, "/");
-            case '=': position++; return new Token(Token.Type.EQUALS,"=");
-            case '(': position++; return  new Token(Token.Type.LBRACKET,"(");
-            case ';': position++; return  new Token(Token.Type.SEMICOLON,";");
-            case ')': position++; return  new Token(Token.Type.RBRACKET,")");
-            case '}': position++; return new Token(Token.Type.RCURL,"}");
-            case '{': position++; return new Token(Token.Type.LCURL,"{");
+            case '+': position++;return makeToken(Token.Type.PLUS, "+");
+            case '-': position++; return makeToken(Token.Type.MINUS, "-");
+            case '*': position++; return makeToken(Token.Type.STAR, "*");
+            case '/': position++; return makeToken(Token.Type.SLASH, "/");
+            case '=': position++; return makeToken(Token.Type.EQUALS,"=");
+            case '(': position++; return  makeToken(Token.Type.LBRACKET,"(");
+            case ';': position++; return  makeToken(Token.Type.SEMICOLON,";");
+            case ')': position++; return  makeToken(Token.Type.RBRACKET,")");
+            case '}': position++; return makeToken(Token.Type.RCURL,"}");
+            case '{': position++; return makeToken(Token.Type.LCURL,"{");
 
-         }
+        }
         throw new RuntimeException("Write someting meaningfukl"+current);
     }
 }
--- Compiler/src/Main.java
+++ Compiler/src/Main.java
@@ -1,34 +1,25 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("write code here:)");
-        String file_location="/home/deadpool/Working_Project/first_program.txt";
-        BufferedReader reader=new BufferedReader(new FileReader(file_location));
-        String value=reader.readLine();
-        //interpreter hashmap was getting formated every time because it was in loop
-        Interpreter interpreter=new Interpreter();
-        while(value!=null){
-            if(value.trim().isEmpty()){
-                value=reader.readLine();
-                continue;
-            }
-            Lexer lexer = new Lexer(value);
+        String file_location = "/home/deadpool/Working_Project/first_program.txt";
+        StringBuilder sb = new StringBuilder();
+         try (BufferedReader reader = new BufferedReader(new FileReader(file_location))) {
+             String line;
+             while ((line = reader.readLine()) != null) {
+                 sb.append(line).append('\n');
+             }
+         }
+         String source = sb.toString();
+        //from the last time i have taken the whole string becuse making the lexer inside loop make the
+        Lexer lexer = new Lexer(source);
+        Parser parser = new Parser(lexer);
+        List<Stmt> program = parser.parseProgram();
 
-
-
-            Parser parser = new Parser(lexer);
-            List<Stmt> liststmt = parser.parseProgram();
-
-            interpreter.execute(liststmt);
-            value=reader.readLine();
-
-        }
-
-        reader.close();
+        Interpreter interp = new Interpreter();
+        interp.execute(program);
     }
 }
